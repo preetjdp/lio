3416f6f30f2c9698097503ad1af6660c883e4073
Preet Parekh <preetjdp123@gmail.com>
2019-10-06 02:22:37 +0530

    Some basic Refactoring

---
+++ lib/LoadingPage.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+import 'HomePage.dart';
+
+class LoadingPage extends StatefulWidget {
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  VideoPlayerController _videoPlayerController =
+      VideoPlayerController.asset('assets/lio_video.mp4');
+
+  @override
+  void initState() {
+    _videoPlayerController.initialize().then((a) {
+      setState(() {
+        _videoPlayerController.play();
+      });
+    });
+    _videoPlayerController.addListener(() {
+      print(_videoPlayerController.value.position);
+      if (_videoPlayerController.value.position ==
+          _videoPlayerController.value.duration) {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (BuildContext context) => HomePage()));
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(
+                width: _videoPlayerController.value.size?.width ?? 0,
+                height: _videoPlayerController.value.size?.height ?? 0,
+                child: VideoPlayer(_videoPlayerController),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
