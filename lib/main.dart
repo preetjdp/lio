b864aa555488b2835494ab8352d2baa3d1afffa2
Preet Parekh <preetjdp123@gmail.com>
2019-10-05 19:18:33 +0530

    Removed Positioned Button

--- lib/main.dart
+++ lib/main.dart
@@ -70,35 +70,6 @@ class _LoadingPageState extends State<LoadingPage> {
               ),
             ),
           ),
-          Positioned(
-              bottom: 40,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
-                  child: Container(
-                    height: 50,
-                    width: 150,
-                    child: RaisedButton(
-                      color: Colors.white.withOpacity(0.5),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(10.0)),
-                      elevation: 0.0,
-                      onPressed: () {
-                        _videoPlayerController.seekTo(Duration(seconds: 0));
-                      },
-                      child: Text(
-                        'Lets Go',
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black.withOpacity(0.7)),
-                      ),
-                    ),
-                  ),
-                ),
-              )),
         ],
       ),
     );
